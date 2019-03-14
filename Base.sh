c6fae24e96fa04646c05e0830757c1bed9de1683
Diogo Pratas <pratas@ua.pt>
2019-03-14 18:25:11 +0000

    base base

---
+++ Base.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+GET_TOOLS=1;
+#
+GET_FALCON=1;
+GET_GULL=1;
+GET_GOOSE=1;
+GET_KESTREL=1;
+GET_GECO=1;
+GET_AC=1;
+GET_CRYFA=1;
+GET_GECO=1;
+GET_CHESTER=1;
+#
+#
+#==============================================================================
+# GET FALCON
+#
+if [[ "$GET_TOOLS" -eq "1" ]]; then
+  sudo apt-get install terminator;
+  sudo apt-get install git;
+fi
+#
+#==============================================================================
+# GET FALCON
+#
+if [[ "$GET_FALCON" -eq "1" ]]; then
+  rm -fr falcon FALCON FALCON-* *.pl
+  git clone https://github.com/pratas/falcon.git
+  cd falcon/src/
+  cmake .
+  make
+  cp FALCON ../../
+  cp FALCON-FILTER ../../
+  cp FALCON-EYE ../../
+  cd ../../
+  cp falcon/scripts/*.pl .
+fi
+#==============================================================================
+# GET GULL
+#
+if [[ "$GET_GULL" -eq "1" ]]; then
+  rm -fr GULL/ GULL-map GULL-visual
+  git clone https://github.com/pratas/GULL.git
+  cd GULL/src/
+  cmake .
+  make
+  cp GULL-map ../../
+  cp GULL-visual ../../
+  cd ../../
+fi
+#==============================================================================
+# GET GOOSE
+#
+if [[ "$GET_GOOSE" -eq "1" ]]; then
+  rm -fr goose/ goose-*
+  git clone https://github.com/pratas/goose.git
+  cd goose/src/
+  make
+  cp goose-* ../../
+  cd ../../
+fi
+#==============================================================================
+# GET KESTREL
+#
+if [[ "$GET_MAGNET" -eq "1" ]]; then
+  rm -rf Magnet/ MAGNET
+  git clone https://github.com/pratas/magnet.git
+  cd Magnet/src/
+  cmake .
+  make
+  cp Magnet ../../
+  cd ../../
+fi
+#==============================================================================
+# GET ...
+

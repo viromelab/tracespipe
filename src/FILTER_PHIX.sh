032a35911a928e05ddd5cc476a908fccc9e8823d
pratas <pratas@ua.pt>
2019-04-29 16:18:25 +0300

    PHIX filtering

---
+++ src/FILTER_PHIX.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+gunzip o_fw_pr.fq.gz 
+gunzip o_rv_pr.fq.gz 
+gunzip o_fw_unpr.fq.gz 
+gunzip o_rv_unpr.fq.gz
+#
+## REMOVE PHIX FROM THE SAMPLES
+echo "RUNNING MAGNET ...";
+MAGNET -v -F -n 12 -m 6:1:1:0/0 -m 13:20:1:3/10 -t 0.9 -i -o NP-o_fw_pr.fq o_fw_pr.fq
+MAGNET -v -F -n 12 -m 6:1:1:0/0 -m 13:20:1:3/10 -t 0.9 -i -o NP-o_rv_pr.fq o_rv_pr.fq
+MAGNET -v -F -n 12 -m 6:1:1:0/0 -m 13:20:1:3/10 -t 0.9 -i -o NP-o_fw_unpr.fq o_fw_unpr.fq
+MAGNET -v -F -n 12 -m 6:1:1:0/0 -m 13:20:1:3/10 -t 0.9 -i -o NP-o_rv_unpr.fq o_rv_unpr.fq
+#

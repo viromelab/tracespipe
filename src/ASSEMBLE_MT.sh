b1ff7ef9918181852a43e31de7e6790067758f08
pratas <pratas@ua.pt>
2019-04-30 10:19:33 +0300

    assembly

--- src/ASSEMBLE_MT.sh
+++ src/ASSEMBLE_MT.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-spades.py -t 12 -m 20 -o out_spades_$1 MT-o_fw_pr.fq MT-o_rv_pr.fq MT-o_fw_unpr.fq MT-o_rv_unpr.fq
+spades.py -t 12 -m 20 --careful -o out_spades_$1 -1 MT-o_fw_pr.fq -2 MT-o_rv_pr.fq -s MT-o_fw_unpr.fq -s MT-o_rv_unpr.fq
 #

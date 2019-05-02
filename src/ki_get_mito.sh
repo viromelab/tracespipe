880688a41093434f36a9911a9efc9ad7f053efa1
pratas <pratas@ua.pt>
2019-05-02 10:30:52 +0300

    exclude files except the sh

--- src/ki_get_mito.sh
+++ src/ki_get_mito.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #
 rm -f hs_ref_GRCh38.p12_chrMT.fa.gz
+#
+echo "Downloading Reference Human mithocondrial genome (mtDNA.fa) from NCBI ..."
+#
 wget ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/H_sapiens/Assembled_chromosomes/seq/hs_ref_GRCh38.p12_chrMT.fa.gz
 gunzip hs_ref_GRCh38.p12_chrMT.fa.gz
 mv hs_ref_GRCh38.p12_chrMT.fa mtDNA.fa
 #
+echo "Done!";
--- src/ki_get_phix.sh
+++ src/ki_get_phix.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-#  
-./gto_fasta_extract_read_by_pattern -p "phiX174" < VDB.fa > F_PHIX.fa
 #
+echo "Extracting PhiX genomes from VDB.fa into F_PHIX.fa ...";
+#
+gto_fasta_extract_read_by_pattern -p "phiX174" < VDB.fa > F_PHIX.fa
+#
+echo "Done!";

3742664af05a60c4c4bf6f181f99513e8d88c5d1
pratas <pratas@ua.pt>
2019-05-17 11:25:51 +0300

    specific meta

--- src/ki_metagenomics.sh
+++ src/ki_metagenomics.sh
@@ -10,15 +10,15 @@ TOP_SIZE="$3";
 RNAMES="NP-o_fw_pr.fq:NP-o_fw_unpr.fq:NP-o_rv_pr.fq:NP-o_rv_unpr.fq";
 
 ## RUN METAGENOMIC COMPOSITION
-FALCON -v -n 12 -t $TOP_SIZE -F -Z -m 6:1:1:0/0 -m 11:50:1:0/0 -m 18:500:1:5/10 -g 0.85 -c 70 -x top-$ORGAN.csv -y $ORGAN.com $RNAMES $DB
+FALCON -v -n 12 -t $TOP_SIZE -F -Z -m 6:1:1:0/0 -m 11:50:1:0/0 -m 18:500:1:5/10 -g 0.85 -c 70 -x top-non-viral-$ORGAN.csv -y $ORGAN.com $RNAMES $DB
 FALCON-filter -v -F -t 1.0 -o $ORGAN.pos $ORGAN.com
-FALCON-filter-visual -v -e 1 -F -o $ORGAN.svg $ORGAN.pos
+FALCON-filter-visual -v -e 1 -F -o NV-$ORGAN.svg $ORGAN.pos
 
 ## CONVERT SVG OUTPUT TO PDF
-rsvg-convert -f pdf -o $ORGAN.pdf $ORGAN.svg
+rsvg-convert -f pdf -o NV-$ORGAN.pdf NV-$ORGAN.svg
 
 ## RUN GULL FOR INTER-GENOMIC SIMILARITY ANALYSIS
-head -n 40 top-$ORGAN.csv | awk '{ if($3 > 1) print $1"\t"$2"\t"$3"\t"$4; }' \
+head -n 40 top-non-viral-$ORGAN.csv | awk '{ if($3 > 1) print $1"\t"$2"\t"$3"\t"$4; }' \
 | awk '{ print $4;}' | tr '|' '\t' | tr '_' '\t' | awk '{ print $1;}' > GIS-$ORGAN;
 idx=0;
 cat GIS-$ORGAN | while read line
@@ -34,8 +34,8 @@ cat GIS-$ORGAN | while read line
   ((idx++));
   done
 FALCON-inter -v -m 6:1:1:0/0 -m 13:20:1:3/10 -m 20:100:1:5/10 -c 30 -n 8 -x $ORGAN-MATRIX.csv `cat $ORGAN-FNAMES.fil`
-FALCON-inter-visual -v -w 25 -a 8 -x $ORGAN-HEAT.svg $ORGAN-MATRIX.csv
+FALCON-inter-visual -v -w 25 -a 8 -x NV-$ORGAN-HEAT.svg $ORGAN-MATRIX.csv
 
 ## CONVERT SVG OUTPUT TO PDF
-rsvg-convert -f pdf -o $ORGAN-HEAT.pdf $ORGAN-HEAT.svg
+rsvg-convert -f pdf -o NV-$ORGAN-HEAT.pdf NV-$ORGAN-HEAT.svg
 

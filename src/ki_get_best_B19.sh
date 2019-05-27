73fd4e48eb668cbc9cdacb7d82b137d5846f1f9d
pratas <pratas@ua.pt>
2019-05-27 16:10:56 +0300

    get best HV4

--- src/ki_get_best_B19.sh
+++ src/ki_get_best_B19.sh
@@ -3,7 +3,7 @@ ORGAN=$1;
 #
 cat top-$ORGAN.csv \
 | grep -e "B19" -e "parvovirus" -e "Erythrovirus"  \
-| awk '{ if($3 > 1 && $2 > 1500 && $2 < 5000) print $3"\t"$4; }' \
+| awk '{ if($3 > 1 && $2 > 4000 && $2 < 6000) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $2;}' \
 | tr '_' '\t' \

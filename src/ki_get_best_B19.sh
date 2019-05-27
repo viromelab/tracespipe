d77d2f75b4ef37b60e5edc243361cc6fc243c936
pratas <pratas@ua.pt>
2019-05-27 16:01:16 +0300

    best B19

---
+++ src/ki_get_best_B19.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ORGAN=$1;
+#
+cat top-$ORGAN.csv \
+| grep -e "B19" -e "parvovirus" -e "Erythrovirus"  \
+| awk '{ if($3 > 1 && $2 > 1500 && $2 < 5000) print $3"\t"$4; }' \
+| head -n 1 \
+| awk '{ print $2;}' \
+| tr '_' '\t' \
+| awk '{ print $1;}'

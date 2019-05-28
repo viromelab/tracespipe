58f9c4a6806f7239aa2b3099fc2867a4edbc89bc
pratas <pratas@ua.pt>
2019-05-28 10:02:12 +0300

    HHV3 added

---
+++ src/ki_get_best_HV3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ORGAN=$1;
+#
+cat top-$ORGAN.csv \
+| grep -e "herpesvirus_3" -e "HHV-3"  \
+| awk '{ if($3 > 1 && $2 > 100000 && $2 < 300000) print $3"\t"$4; }' \
+| head -n 1 \
+| awk '{ print $2;}' \
+| tr '_' '\t' \
+| awk '{ print $1;}'
--- src/ki_get_best_HV4.sh
+++ src/ki_get_best_HV4.sh
@@ -2,7 +2,7 @@
 ORGAN=$1;
 #
 cat top-$ORGAN.csv \
-| grep -e "Epstein" -e "epstein" -e "herpesvirus_4" -e "gammaherpesvirus_4" \
+| grep -e "Epstein" -e "epstein" -e "herpesvirus_4"" \
 | awk '{ if($3 > 1 && $2 > 110000 && $2 < 300000) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $2;}' \

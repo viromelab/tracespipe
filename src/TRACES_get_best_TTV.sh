dc60a366bdc1cbf475d160a2aa543f20dc012139
pratas <pratas@ua.pt>
2019-06-06 09:35:50 +0300

    expression added to TTV

--- src/TRACES_get_best_TTV.sh
+++ src/TRACES_get_best_TTV.sh
@@ -2,7 +2,7 @@
 ORGAN=$1;
 #
 RESULT=`cat top-$ORGAN.csv \
-| grep -a -e "nello" -e "orque" \
+| grep -a -e "_TT_virus" -e "anello" -e "Anello" -e "torque" -e "Torque" \
 | awk '{ if($3 > 0 && $2 > 2000 && $2 < 5000) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $1"\t"$2;}' \

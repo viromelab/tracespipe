14e63c07cf7dfa9f465ba09d9e474b3329541868
pratas <pratas@ua.pt>
2019-06-06 11:08:32 +0300

    efetch x174 instead of filter

--- src/TRACES_get_phix.sh
+++ src/TRACES_get_phix.sh
@@ -2,6 +2,7 @@
 #
 echo "Extracting PhiX genomes from VDB.fa into F_PHIX.fa ...";
 #
-gto_fasta_extract_read_by_pattern -p "phiX174" < VDB.fa > F_PHIX.fa
+#gto_fasta_extract_read_by_pattern -p "phiX174" < VDB.fa > F_PHIX.fa
+efetch -db nucleotide -format fasta -id "J02482.1,NC_001422.1" > F_PHIX.fa
 #
 echo "Done!";

856c03f1f55f45221deae314a763659806ad8146
pratas <pratas@ua.pt>
2019-06-11 11:15:16 +0300

    info msg

--- src/TRACES_get_phix.sh
+++ src/TRACES_get_phix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-echo "Extracting PhiX genomes from VDB.fa into F_PHIX.fa ...";
+echo "Downloading Phix reference genomes (F_PHIX.fa) from NCBI ...";
 #
 #gto_fasta_extract_read_by_pattern -p "phiX174" < VDB.fa > F_PHIX.fa
 efetch -db nucleotide -format fasta -id "J02482.1,NC_001422.1" > F_PHIX.fa

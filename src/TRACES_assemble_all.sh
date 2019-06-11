e3a79e2fa00ea641ef45f925ab2e3b116d98d7da
pratas <pratas@ua.pt>
2019-06-11 11:07:37 +0300

    inscreasing max size of SPADES RAM

--- src/TRACES_assemble_all.sh
+++ src/TRACES_assemble_all.sh
@@ -4,5 +4,5 @@
 # o_fw_pr.fq o_rv_pr.fq o_fw_unpr.fq o_rv_unpr.fq
 #
 # ASSEMBLE
-spades.py -t 12 -m 20 --careful -o out_spades_full_$1 -1 o_fw_pr.fq -2 o_rv_pr.fq -s o_fw_unpr.fq -s o_rv_unpr.fq
+spades.py -t 12 -m 46 --careful -o out_spades_full_$1 -1 o_fw_pr.fq -2 o_rv_pr.fq -s o_fw_unpr.fq -s o_rv_unpr.fq
 #

ea3e97c2d7fbef8cea44573f04cf74ce4ddff0f2
Pratas <pratas@ua.pt>
2019-06-21 16:39:36 +0300

    system files for add extra seq

--- src/TRACES_get_enriched_sequences.sh
+++ src/TRACES_get_enriched_sequences.sh
@@ -2,6 +2,6 @@
 rm -f en_seqs.fa;
 while read line; do
 efetch -db nucleotide -format fasta -id $line >> en_seqs.fa
-done < ids_enrichment.txt 
+done < ../system_files/ids_enrichment.txt 
 cat $1 en_seqs.fa > TMP_VDB.fa
 mv TMP_VDB.fa $1

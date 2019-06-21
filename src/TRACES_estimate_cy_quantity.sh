4832e012b9a4e43642530f4b49fd33feb89f5f77
Pratas <pratas@ua.pt>
2019-06-21 19:50:16 +0300

    filter only CY

--- src/TRACES_estimate_cy_quantity.sh
+++ src/TRACES_estimate_cy_quantity.sh
@@ -3,9 +3,11 @@
 # IT ASSUMES THAT THE FOLLOWING OUTPUT FILES EXIST:
 # o_fw_pr.fq o_fw_unpr.fq o_rv_pr.fq o_rv_unpr.fq
 #
-cat o_fw_pr.fq o_rv_pr.fq o_fw_unpr.fq o_rv_unpr.fq > REFERENCE_CY.fq
+cat o_fw_pr.fq o_rv_pr.fq o_fw_unpr.fq o_rv_unpr.fq > READS_TO_CY.fq
 #
-gto_geco -v -rm 8:1:1:0:0.9/0:0:0 -rm 14:20:1:0:0.94/0:0:0 -rm 16:200:1:30:0.95/5:10:0.95 -r REFERENCE_CY.fq cy.fa > REP_CY_$1.txt
+MAGNET -v -F -n 12 -m 6:1:1:0/0 -m 13:20:1:3/1 -m 14:20:1:5/1 -t 0.3 -o READS_CY_FILTERED_cy.fq READS_TO_CY.fq
 #
-rm -f REFERENCE_CY.fq;
+gto_geco -v -rm 8:1:1:0:0.9/0:0:0 -rm 14:20:1:0:0.94/0:0:0 -rm 16:200:1:30:0.95/5:10:0.95 -r READS_CY_FILTERED_cy.fq cy.fa > REP_CY_$1.txt
+#
+rm -f READS_TO_CY.fq READS_CY_FILTERED_cy.fq;
 #

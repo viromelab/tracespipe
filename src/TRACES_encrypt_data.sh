f02e448c8a95047967fb8a1dc4fafe84f22d0611
Pratas <pratas@ua.pt>
2019-06-24 22:33:03 +0300

    encrypt data script

--- src/TRACES_encrypt_data.sh
+++ src/TRACES_encrypt_data.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #
-index_f="../encrypted_data/index.txt";
-for file in ../encrypted_data/*
+index_f="../to_encrypt_data/index.txt";
+echo "Please, enter the password to encrypt the files contained in ../to_encrypt_data directory: ";
+read -s password
+echo "$password" > key.txt
+for file in ../to_encrypt_data/*
   do
   if [[ "$file" == "$index_f" ]];
   then
   echo "Jumping index file ($file)";
   else 
-  echo "Decrypting $file ...";
+  echo "Encrypting $file ...";
   out_file=$(basename $file);
-  echo "Please, enter the password: ";
-  read -s password
-  echo "$password" > key.txt
-  cryfa -k key.txt -d $file > ../decrypted_data/$out_file.dec
-  rm -f key.txt;
+  cryfa -k key.txt $file > ../encrypted_data/$out_file.enc
   fi
   done
+rm -f key.txt
 echo "Done!";
 #

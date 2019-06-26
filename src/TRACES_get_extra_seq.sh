f3c819e1784b8dbafa076d789d68165d3f7d44d4
pratas <pratas@ua.pt>
2019-06-26 11:38:12 +0300

    fixed sequence add

--- src/TRACES_get_extra_seq.sh
+++ src/TRACES_get_extra_seq.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 #
 gto_fasta_extract_read_by_pattern -p "$2" < $1 > tmp_exists_or_not.fa
-FIRST_LINE=`head -n 1 tmp_exists_or_not.fa > tmp_first_line.fa`;
-if [ `wc -l tmp_exists_or_not.fa` -ge "2" ]
+FIRST_LINE=`head -n 1 tmp_exists_or_not.fa`;
+NUMBER_OF_LINES=`wc -l tmp_exists_or_not.fa |awk '{print $1;}'`;
+if [ $NUMBER_OF_LINES -ge "2" ]
 then
-  echo "ERROR: This sequence seems to already exist in the VDB.fa.";
+  echo -e "\e[31mERROR: This sequence seems to already exist in the VDB.fa.\e[0m"
   echo "Header: $FIRST_LINE";
   exit 1;
+else
+  rm -f en_seqs.fa;
+  efetch -db nucleotide -format fasta -id $2 > en_seqs.fa
+  NUMBER_OF_DW_LINES=`wc -l en_seqs.fa |awk '{print $1;}'`;
+  if [ $NUMBER_OF_DW_LINES -ge "2" ]
+    then
+    echo "Adding sequence ...";
+    cat $1 en_seqs.fa > TMP_VDB.fa.tmp
+    mv TMP_VDB.fa.tmp $1
+    echo "Done!";
+    fi
 fi
-rm -f en_seqs.fa;
-efetch -db nucleotide -format fasta -id $2 > en_seqs.fa
-cat $1 en_seqs.fa > TMP_VDB.fa
-mv TMP_VDB.fa $1
 #

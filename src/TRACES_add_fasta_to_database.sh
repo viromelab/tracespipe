004a51edd4f2521139b00ba909cbbf8b88cfa950
pratas <pratas@ua.pt>
2019-06-27 11:52:07 +0300

    fasta file add option

--- src/TRACES_add_fasta_to_database.sh
+++ src/TRACES_add_fasta_to_database.sh
@@ -2,13 +2,16 @@
 #
 # check if $2
 FIRST_SYMBOL=`head -c 1 $2`;
-if [ $FIRST_SYMBOL == '>' ]
+#echo "First symbol: $FIRST_SYMBOL";
+#
+if [ "$FIRST_SYMBOL" = ">" ]
   then 
-  echo "It is a fasta file";
+  echo "Adding the FASTA file to the $1 database";
+  cat $1 $2 > tmp-add-fasta-$1
+  mv tmp-add-fasta-$1 $1
+  echo "Done!"
   else
   echo "ERROR: the file $2 is not a FASTA file!";
+  exit 1;
   fi
-#
-#cat $1 $2 > tmp-add-fasta-$1
-#mv tmp-add-fasta-$1 $1
 #

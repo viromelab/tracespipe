d342e842d85fc7252abcfa1b02ca5e3845975e7c
pratas <pratas@ua.pt>
2019-07-16 14:53:12 +0300

    Extract all B19 REF from VDB

---
+++ src/TRACES_extract_all_human_B19.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+cat VDB.fa | grep -a -e "B19 " -e "parvovirus" -e "rythrovirus" | grep -a -v "olyomavirus" | grep -a -v "otavirus" | grep uman | tr ' ' '\t' | awk '{print $1;}' | sed 's/^.//' > access_codes_b19.txt
+mapfile -t CODES < access_codes_b19.txt
+#
+rm -f B19_REFS.fa;
+for xcode in "${CODES[@]}" # 
+  do
+  echo "Extracting $xcode on VDB.fa..."
+  gto_fasta_extract_read_by_pattern -p "$xcode" < VDB.fa >> B19_REFS.fa
+  echo " ";
+  done
+#
+#

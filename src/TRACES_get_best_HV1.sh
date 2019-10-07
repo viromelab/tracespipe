0f99735a4531da684fc10822b9de957a5ac0c20e
Pratas <pratas@ua.pt>
2019-10-07 15:06:56 +0100

    expression for hv1

--- src/TRACES_get_best_HV1.sh
+++ src/TRACES_get_best_HV1.sh
@@ -2,7 +2,7 @@
 ORGAN=$1;
 #
 RESULT=`cat top-$ORGAN.csv \
-| grep -a -e "HHV-1" -e "HHV1" -e "HSV1" -e "HSV-1" -e "erpesvirus_1"  \
+| grep -a -e "HHV-1" -e "HHV1" -e "HSV1" -e "HSV-1" -e "uman herpesvirus_1" -e "uman_herpesvirus_1" -e "uman herpesvirus 1" \
 | awk '{ if($3 > 0 && $2 > 100000 && $2 < 300000) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $1"\t"$2;}' \

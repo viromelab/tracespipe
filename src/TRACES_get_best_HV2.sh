6033a2c0be6edfc3131e8a0ab14f36d1b025985d
Pratas <pratas@ua.pt>
2019-10-07 15:10:52 +0100

    best for HV2

--- src/TRACES_get_best_HV2.sh
+++ src/TRACES_get_best_HV2.sh
@@ -2,7 +2,7 @@
 ORGAN=$1;
 #
 RESULT=`cat top-$ORGAN.csv \
-| grep -a -e "HHV-2" -e "HHV2" -e "HSV2" -e "HSV-2" -e "erpesvirus_2"  \
+| grep -a -e "HHV-2" -e "HHV2" -e "HSV2" -e "HSV-2" -e "uman herpesvirus_2" -e "uman_herpesvirus_2" -e "uman herpesvirus 2"  \
 | awk '{ if($3 > 0 && $2 > 100000 && $2 < 300000) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $1"\t"$2;}' \

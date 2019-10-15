e4738fb05ce033ed50e39071ba99a511dc2179c3
Pratas <pratas@ua.pt>
2019-10-15 14:12:20 +0100

    Hepatitis minimun size

--- src/TRACES_get_best_HBV.sh
+++ src/TRACES_get_best_HBV.sh
@@ -3,7 +3,7 @@ ORGAN=$1;
 #
 RESULT=`cat top-$ORGAN.csv \
 | grep -a -e "hepatitis" -e "Hepatitis"   \
-| awk '{ if($3 > 0 && $2 > 3000 && $2 < 16000) print $3"\t"$4; }' \
+| awk '{ if($3 > 0 && $2 > 1500 && $2 < 6000) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $1"\t"$2;}' \
 | tr '_' '\t' \

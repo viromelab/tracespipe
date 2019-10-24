ab7808ba1d8ac5a8c29c3e862941a97932245af7
Pratas <pratas@ua.pt>
2019-10-24 17:28:53 +0100

    horizontal coverage in percentage

--- src/TRACES_overall_virus.sh
+++ src/TRACES_overall_virus.sh
@@ -9,6 +9,6 @@ NORMALIZED_COVERAGE=`echo "scale=4; $TOTAL_COVERAGE / $TOTAL_SIZE" | bc -l`;
 printf "$VIRUS\t$ORGAN\t$NORMALIZED_COVERAGE\n" > ../output_data/TRACES_viral_statistics/$VIRUS-total-depth-coverage-$ORGAN.txt;
 #
 ZERO_COVERAGE=`awk '{sum += ($3-$2)} END {print sum}' ../output_data/TRACES_viral_bed/$VIRUS-zero-coverage-$ORGAN.bed `;
-NORMALIZED_ZERO_COVERAGE=`echo "scale=4; $ZERO_COVERAGE / $TOTAL_SIZE" | bc -l`;
+NORMALIZED_ZERO_COVERAGE=`echo "scale=4; (100-(($ZERO_COVERAGE / $TOTAL_SIZE)*100))" | bc -l`;
 printf "$VIRUS\t$ORGAN\t$NORMALIZED_ZERO_COVERAGE\n" > ../output_data/TRACES_viral_statistics/$VIRUS-total-horizontal-coverage-$ORGAN.txt;
 #

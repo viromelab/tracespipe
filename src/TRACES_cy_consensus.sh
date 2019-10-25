33d544f525fbe036ac03214931c4001317be404c
Pratas <pratas@ua.pt>
2019-10-25 09:50:39 +0100

    labels for muti runs

--- src/TRACES_cy_consensus.sh
+++ src/TRACES_cy_consensus.sh
@@ -17,21 +17,21 @@ awk '$4 < 1' cy-coverage-$Organ.bed > cy-zero-coverage-$Organ.bed
 #
 # CALLS W CONSENSUS
 samtools faidx $Reference # -P 9.9e-1
-bcftools mpileup -Ou -f $Reference $Alignments | bcftools call --ploidy 1 -P 9.9e-1 -mv -Oz -o calls.vcf.gz
-bcftools index calls.vcf.gz
+bcftools mpileup -Ou -f $Reference $Alignments | bcftools call --ploidy 1 -P 9.9e-1 -mv -Oz -o calls-cy-$Organ.vcf.gz
+bcftools index calls-cy-$Organ.vcf.gz
 #
 # normalize indels
-bcftools norm -f $Reference calls.vcf.gz -Oz -o calls.norm.vcf.gz
+bcftools norm -f $Reference calls-cy-$Organ.vcf.gz -Oz -o calls-cy-$Organ.norm.vcf.gz
 #
 # filter adjacent indels within 5bp
-bcftools filter --IndelGap 5 calls.norm.vcf.gz -Oz -o calls.norm.flt-indels.vcf.gz
+bcftools filter --IndelGap 5 calls-cy-$Organ.norm.vcf.gz -Oz -o calls-cy-$Organ.norm.flt-indels.vcf.gz
 #
 # create bed file
-zcat calls.norm.flt-indels.vcf.gz |vcf2bed --snvs > cy-calls-$Organ.bed
+zcat calls-cy-$Organ.norm.flt-indels.vcf.gz |vcf2bed --snvs > cy-calls-$Organ.bed
 #
 # CONSENSUS
-tabix calls.norm.flt-indels.vcf.gz
-bcftools consensus -m cy-zero-coverage-$Organ.bed -f $Reference calls.norm.flt-indels.vcf.gz > cy-consensus-$Organ.fa
+tabix calls-cy-$Organ.norm.flt-indels.vcf.gz
+bcftools consensus -m cy-zero-coverage-$Organ.bed -f $Reference calls-cy-$Organ.norm.flt-indels.vcf.gz > cy-consensus-$Organ.fa
 #
 # Give new header name for the consensus sequence
 tail -n +2 cy-consensus-$Organ.fa > CY_TMP_FILE_$Organ.xki
@@ -40,6 +40,6 @@ cat CY_TMP_FILE_$Organ.xki >> cy-consensus-$Organ.fa
 rm -f CY_TMP_FILE_$Organ.xki;
 #
 #
-rm -f calls.vcf.gz calls.vcf.gz.csi calls.norm.bcf calls.norm.flt-indels.bcf calls.norm.flt-indels.vcf.gz calls.norm.vcf.gz;
+rm -f calls-cy-$Organ.vcf.gz calls-cy-$Organ.vcf.gz.csi calls-cy-$Organ.norm.bcf calls-cy-$Organ.norm.flt-indels.bcf calls-cy-$Organ.norm.flt-indels.vcf.gz calls-cy-$Organ.norm.vcf.gz;
 #
 #

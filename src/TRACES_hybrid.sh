99ecc1d9f4e0cd621506d36aac8dbdd55b75792f
Pratas <pratas@ua.pt>
2019-11-11 17:19:22 +0000

    TR

--- src/TRACES_hybrid.sh
+++ src/TRACES_hybrid.sh
@@ -6,9 +6,9 @@ THREADS="$3";
 ORGAN="$4";
 #
 #
-if [ ! -f "$REF.fa" ];
+if [ ! -f "$ORGAN-$REF.fa" ];
   then
-  echo -e "\e[38;5;208mWARNING: $REF reference file not found!\e[0m"
+  echo -e "\e[38;5;208mWARNING: $ORGAN-$REF.fa reference file not found!\e[0m"
   echo "TIP: before this, run: ./TRACESPipe.sh --run-meta --run-all-v-alig"
   echo -e "\e[38;5;198mDon't panic! This warning may be cause by absence of aligned results\e[0m."
   echo "For addition information, see the instructions at the web page."
@@ -23,14 +23,14 @@ if [ ! -f "$SCAFFOLDS" ];
   fi
 #
 #
-bwa index $REF.fa
-bwa mem -t $THREADS $REF.fa $SCAFFOLDS > scaffolds_aligned_$REF-$ORGAN.sam
+bwa index $ORGAN-$REF.fa
+bwa mem -t $THREADS $ORGAN-$REF.fa $SCAFFOLDS > scaffolds_aligned_$REF-$ORGAN.sam
 samtools sort scaffolds_aligned_$REF-$ORGAN.sam > scaffolds_aligned_sorted_$REF-$ORGAN.bam
 samtools index scaffolds_aligned_sorted_$REF-$ORGAN.bam scaffolds_aligned_sorted_$REF-$ORGAN.bam.bai
 mv scaffolds_aligned_sorted_$REF-$ORGAN.bam ../output_data/TRACES_hybrid_$ORGAN/
 mv scaffolds_aligned_sorted_$REF-$ORGAN.bam.bai ../output_data/TRACES_hybrid_$ORGAN/
-cp $REF.fa ../output_data/TRACES_hybrid_$ORGAN/
-cp $REF.fa.fai ../output_data/TRACES_hybrid_$ORGAN/
+cp $ORGAN-$REF.fa ../output_data/TRACES_hybrid_$ORGAN/
+cp $ORGAN-$REF.fa.fai ../output_data/TRACES_hybrid_$ORGAN/
 #
 rm -f scaffolds_aligned_$REF-$ORGAN.sam;
 #

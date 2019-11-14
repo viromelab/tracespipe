44d4495bca87849d5de17ebc1ade75d07ae20126
Pratas <pratas@ua.pt>
2019-11-14 14:37:57 +0000

    repetitive element fix

--- src/TRACES_coverage_table.sh
+++ src/TRACES_coverage_table.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-declare -a VIRUSES=('B19' 'HV1' 'HV2' 'HV3' 'HV4' 'HV5' 'HV6' 'HV6A' 'HV6B' 'HV7' 'HV8' 'POLY1' 'POLY2' 'POLY2' 'POLY3' 'POLY4' 'POLY5' 'POLY6' 'POLY7' 'POLY8' 'POLY9' 'POLY10' 'POLY11' 'POLY12' 'POLY13' 'POLY14' 'TTV' 'HBOV1' 'HBOVNOT1' 'HBV' 'HPV' 'VARV' 'SV40' 'CUTA' 'HERV');
+declare -a VIRUSES=('B19' 'HV1' 'HV2' 'HV3' 'HV4' 'HV5' 'HV6' 'HV6A' 'HV6B' 'HV7' 'HV8' 'POLY1' 'POLY2' 'POLY3' 'POLY4' 'POLY5' 'POLY6' 'POLY7' 'POLY8' 'POLY9' 'POLY10' 'POLY11' 'POLY12' 'POLY13' 'POLY14' 'TTV' 'HBOV1' 'HBOVNOT1' 'HBV' 'HPV' 'VARV' 'SV40' 'CUTA' 'HERV');
 declare -a ORGANS=('blood' 'bone' 'brain' 'hair' 'heart' 'kidney' 'liver' 'lung' 'marrow' 'skin' 'teeth' );
 #
 x=0;

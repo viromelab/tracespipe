ac1f1547f74279023a57530b7a6fc712a0384eb7
pratas <pratas@ua.pt>
2019-11-21 14:14:34 +0200

    names

--- src/TRACES_get_report_meta.sh
+++ src/TRACES_get_report_meta.sh
@@ -3,9 +3,19 @@ cat ../meta_data/meta_info.txt | tr ':' '\t' | awk '{ print $1}' > names_tmp_TRA
 mapfile -t NAMES < names_tmp_TRACES.tmp
 #
 rm -f names2_tmp_TRACES.tmp
+#
+printf "\t" > names3_tmp_TRACES.tmp;
+#
+for name in "${NAMES[@]}" #
+  do
+  printf "$name\t\t" >> names3_tmp_TRACES.tmp;
+  done
+printf "\n" >> names3_tmp_TRACES.tmp;
+#
 for name in "${NAMES[@]}" #
   do
   printf "../output_data/TRACES_results/REPORT_META_VIRAL_$name.txt " >> names2_tmp_TRACES.tmp
   done
-paste -d "\t" `cat names2_tmp_TRACES.tmp` > ../output_data/TRACES_results/REPORT_META_VIRAL_ALL_SAMPLES.txt
-rm -f names_tmp_TRACES.tmp names2_tmp_TRACES.tmp
+cat names3_tmp_TRACES.tmp > ../output_data/TRACES_results/REPORT_META_VIRAL_ALL_SAMPLES.txt;
+paste -d "\t" viral_names.txt `cat names2_tmp_TRACES.tmp` >> ../output_data/TRACES_results/REPORT_META_VIRAL_ALL_SAMPLES.txt
+rm -f names_tmp_TRACES.tmp names2_tmp_TRACES.tmp names3_tmp_TRACES.tmp

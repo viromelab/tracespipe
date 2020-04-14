bb032523f1b4bdee34614b272dcb720fc6224956
Diogo Pratas <pratas@ua.pt>
2020-04-14 11:53:42 +0100

    max coverage

--- src/TRACES_project_coordinates.sh
+++ src/TRACES_project_coordinates.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
+# TRACES_project_coordinates.sh positions_files max_coverage
+#
 mapfile -t POSITIONS < $1
 #
 for read in "${POSITIONS[@]}" #
@@ -11,6 +13,17 @@ for read in "${POSITIONS[@]}" #
   #
   for(( x = $IPOS ; x < $EPOS ; ++x ));
     do
-    printf "%u\t%u\n" "$x" "$COVE";
+    if [[ "$2" -ne "0" ]];
+      then
+      if [[ "$COVE" -gt "$2" ]];
+        then
+        printf "%u\t%u\n" "$x" "$2";
+        else
+        printf "%u\t%u\n" "$x" "$COVE";
+        fi
+      else
+      printf "%u\t%u\n" "$x" "$COVE";
+      fi
     done
   done
+#

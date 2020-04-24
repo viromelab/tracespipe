1fe95b5695b95795ff806025c4b364ff64853611
Pratas <pratas@ua.pt>
2020-04-25 00:21:52 +0100

    msg encrypting and decrypting

--- src/TRACES_decrypt_data.sh
+++ src/TRACES_decrypt_data.sh
@@ -5,16 +5,16 @@ for file in ../encrypted_data/*
   do
   if [[ "$file" == "$index_f" ]];
   then
-  echo "Jumping index file ($file)";
+  echo -e "\e[34m[TRACESPipe]\e[32m Jumping index file ($file) \e[0m";
   else 
-  echo "Decrypting $file ...";
+  echo -e "\e[34m[TRACESPipe]\e[32m Decrypting $file \e[0m";
   out_file=$(basename $file);
-  echo "Please, enter the password: ";
+  echo -e "\e[34m[TRACESPipe]\e[32m Please, enter the password: \e[0m";
   read -s password
   echo "$password" > key.txt
-  cryfa -k key.txt -d $file > ../decrypted_data/$out_file.dec
+  cryfa -k key.txt -d $file > ../decrypted_data/$out_file.dec 2>> ../logs/Log-stderr-$ORGAN_T.txt;
   rm -f key.txt;
   fi
   done
-echo "Done!";
+echo -e "\e[34m[TRACESPipe]\e[32m Done! \e[0m";
 #
--- src/TRACES_encrypt_data.sh
+++ src/TRACES_encrypt_data.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #
 index_f="../to_encrypt_data/index.txt";
-echo "Please, enter the password to encrypt the files contained in ../to_encrypt_data directory: ";
+echo -e "\e[34m[TRACESPipe]\e[32m Please, enter the password to encrypt the files contained in ../to_encrypt_data directory: \e[0m";
 read -s password
 echo "$password" > key.txt
 for file in ../to_encrypt_data/*
   do
   if [[ "$file" == "$index_f" ]];
   then
-  echo "Jumping index file ($file)";
+  echo -e "\e[34m[TRACESPipe]\e[32m Jumping index file ($file) \e[0m";
   else 
-  echo "Encrypting $file ...";
+  echo -e "\e[34m[TRACESPipe]\e[32m Encrypting $file \e[0m";
   out_file=$(basename $file);
-  cryfa -k key.txt $file > ../encrypted_data/$out_file.enc
+  cryfa -k key.txt $file > ../encrypted_data/$out_file.enc 2>> ../logs/Log-stderr-$ORGAN_T.txt;
   fi
   done
 rm -f key.txt
-echo "Done!";
+echo -e "\e[34m[TRACESPipe]\e[32m Done! \e[0m";
 #

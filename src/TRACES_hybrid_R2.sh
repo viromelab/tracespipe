7df7050034e5e7cd8b85ae07773b6a3c33669328
Pratas <pratas@ua.pt>
2020-04-28 21:12:06 +0100

    def rt

--- src/TRACES_hybrid_R2.sh
+++ src/TRACES_hybrid_R2.sh
@@ -14,16 +14,16 @@ if [ ! -f "$1" ] || [ ! -f "$2" ];
   fi
 #
 bwa index $1
-bwa mem -t $THREADS -I 0 -O 2 -N 0.02 -L 1024 -E 7 $1 $2 > scaffolds_aligned_$VIRUS-$ORGAN.sam 
-samtools sort scaffolds_aligned_$VIRUS-$ORGAN.sam > scaffolds_aligned_sorted_$VIRUS-$ORGAN.bam
-samtools index scaffolds_aligned_sorted_$VIRUS-$ORGAN.bam scaffolds_aligned_sorted_$VIRUS-$ORGAN.bam.bai
-rm -f scaffolds_aligned_$VIRUS-$ORGAN.sam;
+bwa mem -t $THREADS -I 0 -O 2 -N 0.02 -L 1024 -E 7 $1 $2 > hybrid_aligned_$VIRUS-$ORGAN.sam 
+samtools sort hybrid_aligned_$VIRUS-$ORGAN.sam > hybrid_aligned_sorted_$VIRUS-$ORGAN.bam
+samtools index hybrid_aligned_sorted_$VIRUS-$ORGAN.bam hybrid_aligned_sorted_$VIRUS-$ORGAN.bam.bai
+rm -f hybrid_aligned_$VIRUS-$ORGAN.sam;
 #
-bedtools genomecov -ibam scaffolds_aligned_sorted_$VIRUS-$ORGAN.bam -bga > $VIRUS-coverage-$ORGAN.bed
+bedtools genomecov -ibam hybrid_aligned_sorted_$VIRUS-$ORGAN.bam -bga > $VIRUS-coverage-$ORGAN.bed
 awk '$4 < 1' $VIRUS-coverage-$ORGAN.bed > $VIRUS-zero-coverage-$ORGAN.bed
 samtools faidx $1 # -P 9.9e-1                                      # here!
-bcftools mpileup -Ou -f $1 scaffolds_aligned_sorted_$VIRUS-$ORGAN.bam \
-| bcftools call --ploidy 1 -P 9.9e-1 -mv -Oz -o $VIRUS-$ORGAN-calls.vcf.gz
+bcftools mpileup -Ou -f $1 hybrid_aligned_sorted_$VIRUS-$ORGAN.bam \
+| bcftools call --ploidy 1 -mv -Oz -o $VIRUS-$ORGAN-calls.vcf.gz
 bcftools index $VIRUS-$ORGAN-calls.vcf.gz
 bcftools norm -f $1 $VIRUS-$ORGAN-calls.vcf.gz -Oz -o $VIRUS-$ORGAN-calls.norm.vcf.gz
 zcat $VIRUS-$ORGAN-calls.norm.vcf.gz |vcf2bed --snvs > $VIRUS-calls-$ORGAN.bed
@@ -36,12 +36,12 @@ echo ">Hybrid Round2 $VIRUS (organ=$ORGAN) consensus" > $VIRUS-consensus-$ORGAN.
 cat $VIRUS-$ORGAN-TMP2_FILE.xki >> $VIRUS-consensus-$ORGAN.fa
 rm -f $VIRUS-$ORGAN-TMP2_FILE.xki;
 #
-cp $VIRUS-consensus-$ORGAN.fa ../output_data/TRACES_hybrid_R2_consensus/
-cp $VIRUS-coverage-$ORGAN.bed ../output_data/TRACES_hybrid_R2_bed/
-cp $VIRUS-zero-coverage-$ORGAN.bed ../output_data/TRACES_hybrid_R2_bed/
-cp scaffolds_aligned_sorted_$VIRUS-$ORGAN.bam ../output_data/TRACES_hybrid_R2_alignments/
-cp scaffolds_aligned_sorted_$VIRUS-$ORGAN.bam.bai ../output_data/TRACES_hybrid_R2_alignments/
-cp $1 ../output_data/TRACES_hybrid_R2_alignments/
+mv $VIRUS-consensus-$ORGAN.fa ../output_data/TRACES_hybrid_R2_consensus/
+mv $VIRUS-coverage-$ORGAN.bed ../output_data/TRACES_hybrid_R2_bed/
+mv $VIRUS-zero-coverage-$ORGAN.bed ../output_data/TRACES_hybrid_R2_bed/
+mv hybrid_aligned_sorted_$VIRUS-$ORGAN.bam ../output_data/TRACES_hybrid_R2_alignments/
+mv hybrid_aligned_sorted_$VIRUS-$ORGAN.bam.bai ../output_data/TRACES_hybrid_R2_alignments/
+mv $1 ../output_data/TRACES_hybrid_R2_alignments/
 #
 rm -f $VIRUS-$ORGAN-calls.vcf.gz $VIRUS-$ORGAN-calls.vcf.gz.csi $VIRUS-$ORGAN-calls.norm.bcf $VIRUS-$ORGAN-calls.norm.vcf.gz.csi $VIRUS-$ORGAN-calls.norm.vcf.gz;
 #

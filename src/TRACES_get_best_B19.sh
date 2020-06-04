167fa8a8002dbefe7363f5125d4e396f7e7b79ab
Pratas <pratas@ua.pt>
2020-06-04 11:19:56 +0100

    B19 Filtering options

--- src/TRACES_get_best_B19.sh
+++ src/TRACES_get_best_B19.sh
@@ -2,10 +2,15 @@
 ORGAN=$1;
 #
 RESULT=`cat top-$ORGAN.csv \
-| grep -a -e "B19" -e "parvovirus" -e "Erythrovirus" \
-| grep -a -e "complete genome" -e "complete_genome" \
-| grep -a -v "olyomavirus" \
-| awk '{ if($3 > 0 && $2 > 4000 && $2 < 6000) print $3"\t"$4; }' \
+| grep -a -e " B19 " -e "parvovirus" -e "Parvovirus" -e "erythrovirus" -e "Erythrovirus" \
+| grep -a -e "complete genome" -e "complete_genome" -e "Complete genome" -e "Complete_genome" \
+| grep -a -v "Polyomavirus" \ 
+| grep -a -v "polyomavirus" \ 
+| grep -a -v "bocavirus" \
+| grep -a -v "Bocavirus" \
+| grep -a -v "bocaparvovirus" \
+| grep -a -v "Bocaparvovirus" \
+| awk '{ if($3 > 0 && $2 > 5300 && $2 < 5900) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $1"\t"$2;}' \
 | sed "s/NC\_/NC-/" \

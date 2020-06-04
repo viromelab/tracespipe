74ffb9a0ed56f78466c7986d92560e934547b058
Pratas <pratas@ua.pt>
2020-06-04 14:19:08 +0100

    minor B19 change -> 2 DB

--- src/TRACES_get_best_B19.sh
+++ src/TRACES_get_best_B19.sh
@@ -2,7 +2,7 @@
 ORGAN=$1;
 #
 RESULT=`cat top-$ORGAN.csv \
-| grep -a -e " B19 " -e "parvovirus" -e "Parvovirus" -e "erythrovirus" -e "Erythrovirus" \
+| grep -a -e " B19 " -e "_B19_" -e " B19V " -e "_B19V_" -e "parvovirus" -e "Parvovirus" -e "erythrovirus" -e "Erythrovirus" \
 | grep -a -e "complete genome" -e "complete_genome" -e "Complete genome" -e "Complete_genome" \
 | grep -v "Polyomavirus" \
 | grep -v "polyomavirus" \

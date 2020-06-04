81eeae1ca6a4b05d1837051eca897ad8845721da
Pratas <pratas@ua.pt>
2020-06-04 13:57:52 +0100

    unicode

--- src/TRACES_get_best_B19.sh
+++ src/TRACES_get_best_B19.sh
@@ -4,12 +4,12 @@ ORGAN=$1;
 RESULT=`cat top-$ORGAN.csv \
 | grep -a -e " B19 " -e "parvovirus" -e "Parvovirus" -e "erythrovirus" -e "Erythrovirus" \
 | grep -a -e "complete genome" -e "complete_genome" -e "Complete genome" -e "Complete_genome" \
-| grep -a -v "Polyomavirus" \ 
-| grep -a -v "polyomavirus" \ 
-| grep -a -v "bocavirus" \
-| grep -a -v "Bocavirus" \
-| grep -a -v "bocaparvovirus" \
-| grep -a -v "Bocaparvovirus" \
+| grep -v "Polyomavirus" \
+| grep -v "polyomavirus" \
+| grep -v "bocavirus" \
+| grep -v "Bocavirus" \
+| grep -v "bocaparvovirus" \
+| grep -v "Bocaparvovirus" \
 | awk '{ if($3 > 0 && $2 > 5300 && $2 < 5900) print $3"\t"$4; }' \
 | head -n 1 \
 | awk '{ print $1"\t"$2;}' \

8109364b85435adab75816479d12fb0964576c35
Pratas <pratas@ua.pt>
2020-07-19 23:23:53 +0100

    info

--- src/TRACES_blastn_n_db.sh
+++ src/TRACES_blastn_n_db.sh
@@ -7,7 +7,23 @@
 SPATH="../system_files/blast_dbs/nt";
 QUERY="$1";
 DBASE="nt";
+#
 #STYLE="6 qacc sacc pident mismatch btop";
+#
+# qseqid    Query Seq-id
+# sseqid    Subject Seq-id
+# qstart    Start of alignment in query
+# qend      End of alignment in query
+# sstart    Start of alignment in subject
+# send      End of alignment in subject
+# evalue    Expect value
+# bitscore  Bit score
+# length    Alignment length
+# pident    Percentage of identical matches
+# mismatch  Number of mismatches
+# gapopen   Number of gap openings
+# btop      Blast traceback operations (BTOP)
+#
 STYLE="6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore btop";
 #
 # RUN ==========================================================================

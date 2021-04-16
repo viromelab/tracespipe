150a204a2883b0bef1e13ea8342de741200e290b
pratas <pratas@ua.pt>
2021-04-16 12:43:43 +0300

    rescale & rm old files

--- src/TRACES_gid_damage_patterns.sh
+++ src/TRACES_gid_damage_patterns.sh
@@ -18,10 +18,13 @@ CHECK_ADAPTERS_AR () {
 #  
 CHECK_ADAPTERS_AR;
 #
+rm -f $GID.fa* $GID-$ORGAN.sam $GID-$ORGAN.bam FIL-$GID-$ORGAN.bam 
+rm -fr ../output_data/TRACES_damage_$GID-$ORGAN
+#
 efetch -db nucleotide -format fasta -id "$GID" > $GID.fa
 #
 echo -e "\e[34m[TRACESPipe]\e[32m Trimming, filtering, and collapsing with AdapterRemoval ...\e[0m";
-AdapterRemoval --threads $THREADS --file1 FW_READS.fq.gz --file2 RV_READS.fq.gz --outputcollapsed reads.fq --trimns --trimqualities --minlength 30 --collapse --adapter-list adapters_ar.fa # 1>> ../logs/Log-stdout-$ORGAN.txt 2>> ../logs/Log-stderr-$ORGAN.txt;
+AdapterRemoval --threads $THREADS --file1 FW_READS.fq.gz --file2 RV_READS.fq.gz --outputcollapsed reads.fq --trimns --trimqualities --minlength 30 --collapse --adapter-list adapters_ar.fa 1>> ../logs/Log-stdout-$ORGAN.txt 2>> ../logs/Log-stderr-$ORGAN.txt;
 echo -e "\e[34m[TRACESPipe]\e[32m Aligning $GID data using bwa ...\e[0m";
 bwa index $GID.fa 1>> ../logs/Log-stdout-$ORGAN.txt 2>> ../logs/Log-stderr-$ORGAN.txt;
 bwa mem -t $THREADS -I 0 -O 2 -N 0.02 -L 1024 -E 7 $GID.fa reads.fq > $GID-$ORGAN.sam 2>> ../logs/Log-stderr-$ORGAN.txt;
@@ -30,7 +33,6 @@ samtools view -bSh $GID-$ORGAN.sam > $GID-$ORGAN.bam
 samtools view -bh -F4 $GID-$ORGAN.bam > FIL-$GID-$ORGAN.bam;
 echo -e "\e[34m[TRACESPipe]\e[32m Estimating the damage of mtDNA using mapDamage2 ...\e[0m";
 rm -fr ../output_data/TRACES_damage_$GID-$ORGAN
-#mapDamage --rescale -d ../output_data/TRACES_mtdna_damage_$ORGAN_T -i FIL-$ORGAN_T.bam -r mtDNA.fa;
-mapDamage -d ../output_data/TRACES_damage_$GID-$ORGAN -i FIL-$GID-$ORGAN.bam -r $GID.fa 1>> ../logs/Log-stdout-$ORGAN.txt 2>> ../logs/Log-stderr-$ORGAN.txt;
+mapDamage --rescale -d ../output_data/TRACES_damage_$GID-$ORGAN -i FIL-$GID-$ORGAN.bam -r $GID.fa 1>> ../logs/Log-stdout-$ORGAN.txt 2>> ../logs/Log-stderr-$ORGAN.txt;
 echo -e "\e[34m[TRACESPipe]\e[32m Done!\e[0m"
 #

dcccf5e8ed17ef600738809e8451f79294d57c01
Pratas <pratas@ua.pt>
2021-07-01 10:33:45 +0100

    var

--- src/TRACES_ivar.sh
+++ src/TRACES_ivar.sh
@@ -14,9 +14,11 @@ echo "Using Organ       : $Organ";
 echo "Using Viral Label : $Label";
 echo "Threshold         : $Threshold";
 #
+mkdir -p ./output_data/TRACES_results/
+#
 rm -f $SID.gff3;
 SID=`head $Reference | grep ">" | tr -d ">" | awk '{ print $1;}'`;
-wget -O $SID.gff3 "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nucleotide&report=gff3&id=$SID" 2>> .tmp
+wget -O $SID.gff3 "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nucleotide&report=gff3&id=$SID" 2>> ../logs/Log-stderr.txt
 samtools mpileup -aa -A -d 600000 -B -Q 0 $Alignments | ivar variants -p ../output_data/TRACES_results/Variants-$Organ-$Label -q 20 -t $Threshold -r $Reference -g $SID.gff3
 cp $SID.gff3 ../output_data/TRACES_results/Gff-$Organ-$SID.gff3
 #

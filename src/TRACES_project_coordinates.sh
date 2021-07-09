52f08d52aac44c6eea98de18bb4a8f3efc13b482
pratas <pratas@ua.pt>
2021-07-09 12:47:08 +0300

    silly bug cycle

--- src/TRACES_project_coordinates.sh
+++ src/TRACES_project_coordinates.sh
@@ -16,12 +16,13 @@ for read in "${POSITIONS[@]}" #
   EPOS=`echo $read | awk '{ print $3 }'`;
   COVE=`echo $read | awk '{ print $4 }'`;
   #
+  COVE2=`echo $COVE | sed -E 's/([+-]?[0-9.]+)[eE]\+?(-?)([0-9]+)/(\1*10^\2\3)/g' | bc -l`;
+  COVE_PARSED=`printf "%.0f" "$COVE2"`;
+  #
   for((x = $IPOS ; x < $EPOS ; ++x));
     do
     if [[ "$2" -ne "0" ]];
       then
-      COVE2=`echo $COVE | sed -E 's/([+-]?[0-9.]+)[eE]\+?(-?)([0-9]+)/(\1*10^\2\3)/g' | bc -l`;
-      COVE_PARSED=`printf "%.0f" "$COVE2"`;
       if [[ "$COVE_PARSED" -gt "$2" ]];
         then
         printf "%u\t%u\n" "$x" `echo $2`;
@@ -29,7 +30,7 @@ for read in "${POSITIONS[@]}" #
         printf "%u\t%u\n" "$x" "$COVE_PARSED"     
         fi
       else
-      printf "%u\t%u\n" "$x" "$COVE" 
+      printf "%u\t%u\n" "$x" "$COVE_PARSED" 
       fi
     done
   done

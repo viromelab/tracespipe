2a6e07259e12b647bee6d1e9abd88559fb94c7bb
mirakaya <59024914+mirakaya@users.noreply.github.com>
2024-04-15 19:01:06 +0100

    Changes to TRACES_assemble_all.sh.

--- src/TRACES_assemble_all.sh
+++ src/TRACES_assemble_all.sh
@@ -7,5 +7,13 @@
 # o_fw_pr.fq o_rv_pr.fq o_fw_unpr.fq o_rv_unpr.fq
 #
 # ASSEMBLE
-spades.py --meta --threads $THREADS -o ../output_data/TRACES_denovo_$1 -1 o_fw_pr.fq -2 o_rv_pr.fq -s o_fw_unpr.fq -s o_rv_unpr.fq
+if [ -s o_fw_unpr.fq ] && [ -s o_rv_unpr.fq ]; then
+  spades.py --meta --threads $2 -o ../output_data/TRACES_denovo_$1 -1 o_fw_pr.fq -2 o_rv_pr.fq -s o_fw_unpr.fq -s o_rv_unpr.fq
+elif [ -s o_fw_unpr.fq ]; then
+  spades.py --meta --threads $2 -o ../output_data/TRACES_denovo_$1 -1 o_fw_pr.fq -2 o_rv_pr.fq -s o_fw_unpr.fq
+elif [ -s o_rv_unpr.fq ]; then
+  spades.py --meta --threads $2 -o ../output_data/TRACES_denovo_$1 -1 o_fw_pr.fq -2 o_rv_pr.fq -s o_rv_unpr.fq
+else
+  spades.py --meta --threads $2 -o ../output_data/TRACES_denovo_$1 -1 o_fw_pr.fq -2 o_rv_pr.fq
+fi
 #

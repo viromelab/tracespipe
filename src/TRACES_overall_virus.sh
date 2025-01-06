d3b56dec348f112f4c44335c2d98833b0dda1557
Zachery Dickson <zachery.dickson@helsinki.fi>
2025-01-06 22:09:01 +0200

    Updated formatting on new depth calculation format

--- src/TRACES_overall_virus.sh
+++ src/TRACES_overall_virus.sh
@@ -10,7 +10,7 @@ acc=$(head -1 ../output_data/TRACES_viral_bed/$VIRUS-coverage-$ORGAN.bed | cut -
 #Updated Depth calculation which ignores peaks, but more importantly ignores secondary/supplementary alignments
 TOTAL_COVERAGE=$(samtools bedcov --max-depth $MAX_DEPTH <(echo -e "$acc\t0\t$TOTAL_SIZE") ../output_data/TRACES_viral_alignments/viral_aligned_sorted-$ORGAN-$VIRUS.bam | awk '{print $NF}')
 NORMALIZED_COVERAGE=`echo "scale=4; $TOTAL_COVERAGE / $TOTAL_SIZE" | bc -l`;
-printf "$VIRUS\t$ORGAN\t$NORMALIZED_COVERAGE\n" > ../output_data/TRACES_viral_statistics/$VIRUS-total-depth-coverage-$ORGAN.txt;
+printf "$VIRUS\t$ORGAN\t%0.4f\n" "$NORMALIZED_COVERAGE" > ../output_data/TRACES_viral_statistics/$VIRUS-total-depth-coverage-$ORGAN.txt;
 #
 ZERO_COVERAGE=`awk '{sum += ($3-$2)} END {print sum}' ../output_data/TRACES_viral_bed/$VIRUS-zero-coverage-$ORGAN.bed `;
 if [ -z $ZERO_COVERAGE ]
